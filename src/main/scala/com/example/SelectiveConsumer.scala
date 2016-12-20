a8e079bf5d4341406a86c0c40ac4a05ef7387fc0
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-21 00:44:13 +0900

    Rename files

--- src/main/scala/com/example/SelectiveConsumer.scala
+++ src/main/scala/com/example/SelectiveConsumer.scala
@@ -2,7 +2,7 @@ package com.example
 
 import akka.actor._
 
-object SelectiveCustomerDriver extends CompletableApp(3) {
+object SelectiveConsumerDriver extends CompletableApp(3) {
 }
 
 case class MessageTypeA()
