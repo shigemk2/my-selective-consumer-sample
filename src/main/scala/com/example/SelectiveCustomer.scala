a307a5ac5db5c402e1b42fa32db426bb1a804b5c
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-21 00:42:02 +0900

    Add SelectiveConsumer

--- src/main/scala/com/example/SelectiveCustomer.scala
+++ src/main/scala/com/example/SelectiveCustomer.scala
@@ -8,3 +8,14 @@ object SelectiveCustomerDriver extends CompletableApp(3) {
 case class MessageTypeA()
 case class MessageTypeB()
 case class MessageTypeC()
+
+class SelectiveConsumer(
+                         consumerOfA: ActorRef,
+                         consumerOfB: ActorRef,
+                         consumerOfC: ActorRef) extends Actor {
+  def receive = {
+    case message: MessageTypeA => consumerOfA forward message
+    case message: MessageTypeB => consumerOfB forward message
+    case message: MessageTypeC => consumerOfC forward message
+  }
+}
