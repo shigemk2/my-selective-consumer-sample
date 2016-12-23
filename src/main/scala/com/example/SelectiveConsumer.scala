44e73b345558ab48af33ae30dc42b7133ba79cef
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-23 10:30:42 +0900

    Add

--- src/main/scala/com/example/SelectiveConsumer.scala
+++ src/main/scala/com/example/SelectiveConsumer.scala
@@ -3,6 +3,34 @@ package com.example
 import akka.actor._
 
 object SelectiveConsumerDriver extends CompletableApp(3) {
+  val consumerOfA =
+    system.actorOf(
+      Props[ConsumerOfMessageTypeA],
+      "consumerOfA")
+
+  val consumerOfB =
+    system.actorOf(
+      Props[ConsumerOfMessageTypeB],
+      "consumerOfB")
+
+  val consumerOfC =
+    system.actorOf(
+      Props[ConsumerOfMessageTypeC],
+      "consumerOfC")
+
+  val selectiveConsumer =
+    system.actorOf(
+      Props(classOf[SelectiveConsumer],
+        consumerOfA, consumerOfB, consumerOfC),
+      "selectiveConsumer")
+
+  selectiveConsumer ! MessageTypeA()
+  selectiveConsumer ! MessageTypeB()
+  selectiveConsumer ! MessageTypeC()
+
+  awaitCompletion
+
+  println("SelectiveConsumer: completed.")
 }
 
 case class MessageTypeA()
